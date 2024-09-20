7c3d4f097a8c4cea42f6b4455c7a359c846b7116
hopdrea <sult550@gmail.com>
2024-09-20 16:45:16 +0300

    update

--- regr_str.py
+++ regr_str.py
@@ -3,31 +3,66 @@ import numpy as np
 import streamlit as st 
 from skimage import io
 
-st.title(':violet[Сжатие фото] :camera:')
-st.subheader('                              ')
-uploaded_image = st.file_uploader(':violet[Загрузите ваше изображение (обработка ч/б)]', type=['png','jpeg','jpg'])
+st.title(':orange[Построение логистической регрессии]  :chart:')
+st.subheader('***:violet[Допустим датасет с двумя фичами и одним таргетом — в последнем столбце]***')
+file = st.file_uploader(':orange[Загрузите ваш файл csv]', type=['csv'])
 
-if uploaded_image is not None:
-    image_np = io.imread(uploaded_image)[:, :, 0]
+if file is not None:
+    file = pd.read_csv(file)
 else:
     st.stop()
     
+from sklearn.preprocessing import StandardScaler 
+s_scaler = StandardScaler()
+
+file.iloc[:,[0]] = s_scaler.fit_transform(file.iloc[:,[0]])
+file.iloc[:,[1]] = s_scaler.fit_transform(file.iloc[:,[1]])
+   
+def sigmoid(x):
+    sigmoid = 1/(1+np.exp(-x))
+    return sigmoid
+
+class LogReg:
+    def __init__(self, learning_rate, n_iters,n_inputs = 2):
+        self.learning_rate = learning_rate
+        self.n_inputs = n_inputs
+        self.n_iters = n_iters
+        self.coef_ = np.random.uniform(-7, 7, n_inputs)
+        self.intercept_ = np.random.uniform(-7, 7, 1)
+        
+    def fit(self, X, y):
+        n_inputs, n_features = X.shape
+
+        for _ in range(self.n_iters):
+            init_pred = np.dot(X, self.coef_) + self.intercept_
+            sigm_pred = sigmoid(init_pred)
+            grad_w = (-1/n_inputs) * np.dot(X.T, (y - sigm_pred))
+            grad_w0 = (-1/n_inputs) * np.sum(y - sigm_pred)
+            self.coef_  = self.coef_  - self.learning_rate * grad_w
+            self.intercept_ = self.intercept_ - self.learning_rate * grad_w0
+    
+    def predict(self, X):
+        init_pred = np.dot(X, self.coef_) + self.intercept_
+        sigm_pred = sigmoid(init_pred)
+        class_pred = [0 if g<=0.5 else 1 for g in sigm_pred]
+        return class_pred
 try:
     with st.form(key='my_form'):
         submit_button = st.form_submit_button(label='Отправить')
-        top_k=int(st.text_input(label='Напишите меру сжатия (в сингулярных числах)'))   
+        learning_rate=float(st.text_input(label='Напишите желаемый learning rate)'))    
+    with st.form(key='my_form2'):
+        submit_button = st.form_submit_button(label='Отправить')
+        n_iters=int(st.text_input(label='Напишите число желаемых операций)'))
 except:
     st.stop()
-        
-st.image(image_np, caption='Исходное')
-U, sing_values, V = np.linalg.svd(image_np)
-sigma = np.zeros(shape=image_np.shape)
-np.fill_diagonal(sigma, sing_values)
-tr_U = U[:, :top_k]
-tr_sigma = sigma[:top_k, :top_k]
-tr_V = V[:top_k, :]
-final = tr_U@tr_sigma@tr_V
-final_scaled = final - np.min(final)
-final_scaled /= np.max(final_scaled) 
-final_image = (final_scaled * 255).astype(np.uint8)
-st.image(final_image, caption='После сжатия') 
+   
+log_r = LogReg(learning_rate, n_iters)
+log_r.fit(file.iloc[:, [0, 1]], file.iloc[:,2])
+#class_prediction = log_r.predict(file[['Income','CCAvg']])
+
+
+st.subheader('ω0=') 
+st.subheader(log_r.intercept_)  
+st.subheader('ω1,ω2=')
+st.subheader(log_r.coef_)   
+                                         
